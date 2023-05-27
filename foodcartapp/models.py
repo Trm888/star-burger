255e8c4872b99c8de0410b1fd3a005cf4fd8ed4f
tram888 <bigtram888@gmail.com>
2023-05-27 16:24:02 +0900

    Add comment

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -155,7 +155,7 @@ class Order(models.Model):
         ('Доставлен', 'Доставлен'),
     ]
     status = models.CharField('Статус заказа', max_length=50, choices=ORDER_STATUS_CHOICES, default='new', blank=False,)
-
+    comment = models.TextField('Комментарий', max_length=300, blank=True, null=True)
     objects = OrderQuerySet.as_manager()
 
     class Meta:
